d7a246765e40e4b4cdc0324f4ccbb6adab7a3a0d
Giridharan7193 <giridharan7193gs@gmail.com>
2025-04-07 15:14:09 +0530

    Initial Commit

--- src/Components/test.mjs
+++
@@ -1,7 +0,0 @@
-import useLocalStorage from "../hooks/useLocalStorage";
-
-const [theme, setTheme] = useLocalStorage("theme", "dark");
-
-console.log(theme);
-setTheme("light");
-console.log(theme);
